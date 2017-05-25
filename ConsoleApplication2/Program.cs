c5edf6b155a9aa14583659ecd188acc7a3cec9f5
avilavate <avilavate123@gmail.com>
2017-05-25 12:03:11 +0530

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        delegate void Operation(int number);
+        static void Main(string[] args)
+        {
+            Operation op1 = num => { Console.WriteLine(num * 2); };
+            op1 += num => { Console.WriteLine(num * 3); };
+
+            op1(2);
+
+            Console.Read();
+        }
+
+
+    }
+}
